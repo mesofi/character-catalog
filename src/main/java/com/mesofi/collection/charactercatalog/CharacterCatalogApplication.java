25d90c1895819c6f37dc243046be1a6247fc7be2
Armando Rivas Arzaluz <armandorivasarzaluz@Armandos-MacBook-Pro.local>
2022-07-13 11:34:58 -0500

    Added initial project

---
+++ src/main/java/com/mesofi/collection/charactercatalog/CharacterCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mesofi.collection.charactercatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesofi/collection/charactercatalog/CharacterCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesofi.collection.charactercatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cb6bacd9d265975aef7975b7f2b66c0cd9522e9a
Armando Rivas Arzaluz <armandorivasarzaluz@Armandos-MacBook-Pro.local>
2022-08-12 12:26:39 -0500

    Updated unit tests

--- src/test/java/com/mesofi/collection/charactercatalog/MockData.java
+++ src/test/java/com/mesofi/collection/charactercatalog/MockData.java
@@ -1,26 +1,47 @@
 package com.mesofi.collection.charactercatalog;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mesofi.collection.charactercatalog.model.CharacterFigure;
+import com.mesofi.collection.charactercatalog.model.Distribution;
 import com.mesofi.collection.charactercatalog.model.LineUp;
 
 public class MockData {
-    private static List<CharacterFigure> list = new ArrayList<>();
     public static final String SAGA = "Gemini Saga";
     public static final String SAGA_SAGA = "Gemini Saga (God Cloth) Saga Saga premium set";
     public static final String SAGA_GOLD24 = "Gemini Saga GOLD24";
     public static final String LIBRA_SOG = "Libra Dohko (Sacred Cloth)";
 
-    static {
-        list.add(CharacterFigure.builder().lineUp(LineUp.MYTH_CLOTH_EX).name(SAGA).build());
-        list.add(CharacterFigure.builder().lineUp(LineUp.MYTH_CLOTH_EX).name(SAGA_SAGA).build());
-        list.add(CharacterFigure.builder().lineUp(LineUp.MYTH_CLOTH_EX).name(SAGA_GOLD24).build());
-        list.add(CharacterFigure.builder().lineUp(LineUp.MYTH_CLOTH_EX).name(LIBRA_SOG).build());
+    @SuppressWarnings("unchecked")
+    public static List<CharacterFigure> loadAllCharacters(String dbPath) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<LinkedHashMap<String, Object>> characterItems = objectMapper.readValue(new File(dbPath), List.class);
+        return characterItems.stream().map(MockData::mapCharacterFigure).collect(Collectors.toList());
     }
 
-    public static List<CharacterFigure> createBasicEXFigures() {
-        return list;
+    @SuppressWarnings("unchecked")
+    private static CharacterFigure mapCharacterFigure(LinkedHashMap<String, Object> item) {
+        CharacterFigure characterFigure = new CharacterFigure();
+
+        LinkedHashMap<String, String> idMap = (LinkedHashMap<String, String>) item.get("_id");
+        LinkedHashMap<String, Object> releaseDateMap = (LinkedHashMap<String, Object>) item.get("releaseDate");
+        LinkedHashMap<String, Object> dateMap = (LinkedHashMap<String, Object>) releaseDateMap.get("$date");
+
+        characterFigure.setId(idMap.get("$oid"));
+        characterFigure.setName((String) item.get("name"));
+        characterFigure.setReleaseDate(new Date(Long.parseLong((String) dateMap.get("$numberLong"))));
+        characterFigure.setBasePrice(new BigDecimal((String) item.get("basePrice")));
+        characterFigure.setTax(new BigDecimal((String) item.get("tax")));
+        characterFigure.setLineUp(LineUp.valueOf((String) item.get("lineUp")));
+        characterFigure.setDistribution(Distribution.valueOf((String) item.get("distribution")));
+        characterFigure.setUrl((String) item.get("url"));
+
+        return characterFigure;
     }
 }
--- src/test/java/com/mesofi/collection/charactercatalog/service/CharacterFigureServiceTest.java
+++ src/test/java/com/mesofi/collection/charactercatalog/service/CharacterFigureServiceTest.java
@@ -2,7 +2,6 @@ package com.mesofi.collection.charactercatalog.service;
 
 import static com.mesofi.collection.charactercatalog.MockData.SAGA_GOLD24;
 import static com.mesofi.collection.charactercatalog.MockData.SAGA_SAGA;
-import static com.mesofi.collection.charactercatalog.MockData.createBasicEXFigures;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ActiveProfiles;
 import org.yaml.snakeyaml.Yaml;
 
+import com.mesofi.collection.charactercatalog.MockData;
 import com.mesofi.collection.charactercatalog.config.CharacterConfig;
 import com.mesofi.collection.charactercatalog.model.CharacterFigure;
 import com.mesofi.collection.charactercatalog.model.LineUp;
@@ -40,6 +41,9 @@ import com.mesofi.collection.charactercatalog.repository.CharacterUpdatableRepos
 @ExtendWith(MockitoExtension.class)
 public class CharacterFigureServiceTest {
 
+    private static final String DB_PATH = "src/test/resources/mongodb/character-figure.json";
+    private static List<CharacterFigure> allCharacters = new ArrayList<>();
+
     private CharacterFigureService characterFigureService;
 
     @Mock
@@ -49,6 +53,11 @@ public class CharacterFigureServiceTest {
 
     @Value("${character.keyword-exclude}")
     private List<String> ordersBatchSize;
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        allCharacters = MockData.loadAllCharacters(DB_PATH);
+    }
 
     @BeforeEach
     public void init() {
@@ -79,14 +88,14 @@ public class CharacterFigureServiceTest {
         testCharacterFigure(SAGA_GOLD24, name);
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/" + SAGA_SAGA + "/data.csv", numLinesToSkip = 1)
+    // @ParameterizedTest
+    // @CsvFileSource(resources = "/" + SAGA_SAGA + "/data.csv", numLinesToSkip = 1)
     public void isSagaSaga_ShouldReturnTrueForSaga(String name) {
         testCharacterFigure(SAGA_SAGA, name);
     }
 
     private void testCharacterFigure(String expectedName, String actualName) {
-        when(characterRepository.findAllBylineUp(LineUp.MYTH_CLOTH_EX)).thenReturn(createBasicEXFigures());
+        when(characterRepository.findAllBylineUp(LineUp.MYTH_CLOTH_EX)).thenReturn(allCharacters);
 
         // method to be tested
         Optional<CharacterFigure> result = characterFigureService.retrieveCharacterByName(actualName);

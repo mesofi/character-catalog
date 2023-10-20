1232ee9f454c10781ab44b47eb897667232d76b3
Armando Rivas <rivasarmando271084@gmail.com>
2023-10-20 07:38:43 -0600

    Added new colu,n

--- src/main/java/com/mesofi/collection/charactercatalog/views/ListView.java
+++ src/main/java/com/mesofi/collection/charactercatalog/views/ListView.java
@@ -1,5 +1,6 @@
 package com.mesofi.collection.charactercatalog.views;
 
+import java.io.Serial;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -8,6 +9,7 @@ import java.util.Locale;
 import java.util.Objects;
 
 import com.mesofi.collection.charactercatalog.mappers.CharacterFigureModelMapper;
+import com.mesofi.collection.charactercatalog.model.LineUp;
 import com.mesofi.collection.charactercatalog.service.CharacterFigureService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
@@ -22,6 +24,7 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Myth Clothllection")
 @Route(value = "ui")
 public class ListView extends VerticalLayout {
+    @Serial
     private static final long serialVersionUID = 8725171465467462841L;
 
     private final String TBD = "Not published yet";
@@ -37,20 +40,21 @@ public class ListView extends VerticalLayout {
         this.characterFigureService = characterFigureService;
         this.mapper = mapper;
 
+        List<CharacterFigureView> items = characterFigureService.retrieveAllCharacters().stream()
+                .map(mapper::toModelView).toList();
+
         // addClassName("list-view-class");
         setSizeFull();
 
-        configureGrid();
+        configureGrid(items);
         configureForm();
 
         add(getToolBar(), getContent());
-        populateGrid();
+        populateGrid(items);
 
     }
 
-    private void populateGrid() {
-        List<CharacterFigureView> items = characterFigureService.retrieveAllCharacters().stream()
-                .map(mapper::toModelView).toList();
+    private void populateGrid(List<CharacterFigureView> items) {
 
         grid.setItems(items);
     }
@@ -80,7 +84,7 @@ public class ListView extends VerticalLayout {
         return toolBar;
     }
 
-    private void configureGrid() {
+    private void configureGrid(List<CharacterFigureView> items) {
 
         final DateTimeFormatter sStyle = DateTimeFormatter.ofPattern("MMMM, yyyy");
         final DateTimeFormatter lStyle = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
@@ -88,7 +92,15 @@ public class ListView extends VerticalLayout {
         // grid.addClassName("figure-grid-class");
         grid.setSizeFull();
 
-        this.grid.addColumn(CharacterFigureView::getDisplayableName).setHeader("Name").setFlexGrow(0).setSortable(true);
+        this.grid.addColumn(CharacterFigureView::getDisplayableName).setHeader("Name").setFlexGrow(0).setSortable(true)
+                .setFooter(String.format("%d total, %d MC EX, %d MC, %d DD, %d App, %d Crown, %d LOS, %d Figuarts",
+                        items.size(), items.stream().filter($ -> $.getLineUp() == LineUp.MYTH_CLOTH_EX).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.MYTH_CLOTH).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.APPENDIX).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.DDP).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.CROWN).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.LEGEND).count(),
+                        items.stream().filter($ -> $.getLineUp() == LineUp.FIGUARTS).count()));
 
         Locale jpyLocale = new Locale("jp", "JP");
         Locale cnyLocale = new Locale("cn", "CN");
@@ -113,10 +125,9 @@ public class ListView extends VerticalLayout {
                 ? $.getReleaseDate().format($.isReleaseConfirmationDay() ? lStyle : sStyle)
                 : TBD).setHeader("Release Date").setSortable(true).setComparator(CharacterFigureView::getReleaseDate);
 
-        grid.getColumns().stream().forEach($ -> {
-            $.setAutoWidth(true);
-        });
-
+        this.grid.addColumn(CharacterFigureView::getLineUp).setHeader("Line Up").setFlexGrow(0).setSortable(true);
+        
+        grid.getColumns().forEach($ -> $.setAutoWidth(true));
     }
 
     private String unknownDate(LocalDate releaseDate) {
